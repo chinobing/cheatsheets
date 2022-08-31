5cd3401f3cab8535723da3ef8e132156d6a5a128
Bing Wong <wbbady@outlook.com>
2022-08-31 16:02:42 +0800

    update

--- _plugins/jekyll-plugin-highlight-mark.rb
+++ _plugins/jekyll-plugin-highlight-mark.rb
@@ -2,7 +2,7 @@ Jekyll::Hooks.register :documents, :pre_render do |doc|
     # We want to catch all pages, posts, docs in collections
     doc.content = doc.content.gsub(
       /\=\=(.*)\=\=/i,
-      '<mark markdown="span">\1</mark>'
+      '<p markdown="span">\1</p>'
     )
     # setting the markdown="span" flag tells Kramdown to parse what's between the tags, 
     # otherwise it is ignored.
